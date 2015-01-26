6e58641c6ba48c9db12908f83cf7078bf864122e
clement <maboiteaspam@wanadoo.fr>
2015-01-26 23:26:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cozy PouchDB
+
+Plugin to add Pouch DB support to your Cozy-light install.
+
+
+## Install
+
+    cozy-light add-plugin cozy-pouchdb
---
+++ cozy.js
@@ -0,0 +1,24 @@
+
+var Pouchdb = require('pouchdb');
+
+var plugin = {
+  configure: function(options, config, program, cozyLight) {
+    cozyLight.db = new Pouchdb(cozyLight.configHelpers.getHomePath() + '/cozy', {adapter : 'leveldb'});
+  },
+  onExit: function(options, config, callback, cozyLight) {
+    if (cozyLight.db) {
+      try {
+        /*eslint-disable */
+        if(!cozyLight.db._closed) {
+          /*eslint-enable */
+          return cozyLight.db.close(callback);
+        }
+      } catch (err) {
+        cozyLight.logger.error(err);
+      }
+    }
+    callback();
+  }
+};
+
+module.exports = plugin;
